6d502211bf35f8746bde74d744cf22c964103b50
Rodrigo Cardoso <rodrigoscardoso32@gmail.com>
2017-11-17 11:48:39 -0200

    Programa atualizado - Ver. 2.0
     CrtlC CrtlV direto da pasta projetos do VisualStudios 2017

---
+++ JogoCarmemSandiego/JogoCarmemSandiego/FuncoesAdmin.h
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+int cadastroCidade(char nomeCidade[]);
+void cadastroVilao(char vilao[]);
+void cadastroDicas(int nivelDificuldade);
+int cadastrarCaso();
+void editarDados(FILE *admin);
+int opcaoAdmin(char login[]);
